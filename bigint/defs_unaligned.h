0221b4c76d3e93c415144b277a22e4ca2a3fa504
sttr00 <22871489+sttr00@users.noreply.github.com>
2017-11-26 08:16:55 -0500

    ARCH_* macros must not be defined for generic arch

--- bigint/defs_unaligned.h
+++ bigint/defs_unaligned.h
@@ -4,6 +4,14 @@
 #include <platform/unaligned.h>
 #include "defs_word.h"
 
+#ifdef BIGINT_GENERIC_ARCH
+#undef ARCH_X86
+#undef ARCH_X86_64
+#undef ARCH_ARM
+#undef ARCH_ARM64
+#undef ARCH_PPC
+#endif
+
 #if BIGINT_WORD_SIZE == 8
 
 #define bigint_get_unaligned_be get_unaligned64_be
